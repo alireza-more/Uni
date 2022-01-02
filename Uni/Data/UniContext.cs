3fd66d6b9c0e77b7c97fe9c27ac75a6863c249f5
alireza sejdei <alireza.sejdei@gmail.com>
2022-01-02 21:39:14 +0330

    Add project files.

---
+++ Uni/Data/UniContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Uni.Data
+{
+    public class UniContext : DbContext
+    {
+        public UniContext (DbContextOptions<UniContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Uni.Data.User> User { get; set; }
+    }
+}
---
+++ Uni/Data/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uni.Data
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "نام نمیتواند خالی باشد")]
+        public string Name { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [EmailAddress(ErrorMessage = "ایمیل را با فرمت صحیح وارد کنید")]
+        [Required(ErrorMessage = "ایمیل نمیتواند خالی باشد")]
+        public string Email { get; set; }
+
+
+        [Display(Name ="شماره تماس")]
+        [Required(ErrorMessage = "شماره تماس نمیتواند خالی باشد")]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "شماره تماس را با فرمت صحیح وارد کنید")]
+        [Phone(ErrorMessage = "شماره تماس را با فرمت صحیح وارد کنید")]
+        [MinLength(11,ErrorMessage ="شماره تماس را به درستی وارد کنید")]
+        public string Phone { get; set; }
+    }
+}
